b7166ce1ba71198e79cfdee6d92026b30ba54127
Martin Bhuong <martin@genius.ke>
2024-09-08 14:18:13 +0200

    Update to version 1.57.0

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="n8n"
-N8N_VERSION="1.50.1"
+N8N_VERSION="1.57.0"
 
 # Create a new empty container image
 container=$(buildah from scratch)
