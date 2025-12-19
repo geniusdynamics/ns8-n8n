1399b2c5b013b56e23598be530fa72037cf9613a
Kemboi Elvis <kemboielvis22@gmail.com>
2025-12-19 17:24:25 +0300

    feat: updated to version 2

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="n8n"
-N8N_VERSION="1.102.3"
+N8N_VERSION="2.1.1"
 
 # Create a new empty container image
 container=$(buildah from scratch)
